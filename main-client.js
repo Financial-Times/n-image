23cfcdea11b64ef664ea084c4ab5bc4ad69ead74
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-06-08 11:12:27 +0100

    Client JS just exposes lazy load

--- main-client.js
+++ main-client.js
@@ -1,5 +1,3 @@
-import Image from './components/image';
-import Picture from './components/picture';
 import lazyLoad from './src/lib/lazy-load';
 
-export { Image, Picture, lazyLoad };
+export { lazyLoad };
