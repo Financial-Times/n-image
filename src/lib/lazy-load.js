dd2ee1e3e7c31127ac9689de644443721b57b5cd
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-06-09 15:30:52 +0100

    Fix intermittent fading

--- src/lib/lazy-load.js
+++ src/lib/lazy-load.js
@@ -5,7 +5,12 @@ const imageClass = 'n-image';
 const imageLazyLoadingClass = 'n-image--lazy-loading';
 
 const imageHasLoaded = instance =>
-	instance.elements.forEach(img => img.classList.remove(imageLazyLoadingClass));
+	instance.elements.forEach(img =>
+		// NOTE: rather arbitrary pause, needed for the fading to work properly (guessing if the gap between changing
+		// the src/srcset attribtues and the class is too quick, css isn't applied correctly)
+		// if the fading doesn't always work, this might need increasing
+		setTimeout(() => img.classList.remove(imageLazyLoadingClass), 13)
+	);
 
 const loadImage = (img, opts) => {
 	// add the src/srcset attribtues back in
