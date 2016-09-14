092722a654bc601072086a18078bac43d955713f
Rhys Evans <rhys.evans@ft.com>
2016-09-14 16:26:34 +0100

    build shorter, more readable urls for capi images

--- src/helpers/build-image-service-url.js
+++ src/helpers/build-image-service-url.js
@@ -1,5 +1,7 @@
 import qs from 'querystring';
 
+const capiRX = /^http:\/\/com\.ft\.imagepublish\.prod\.s3\.amazonaws\.com\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/
+
 export default (url, params = {}) => {
 	const defaultOptions = {
 		source: 'next',
@@ -7,6 +9,7 @@ export default (url, params = {}) => {
 		compression: 'best'
 	};
 	const options = Object.assign({}, defaultOptions, params);
+	const capiUUID = (capiRX.exec(url) || [])[1];
 
-	return `https://next-geebee.ft.com/image/v1/images/raw/${encodeURIComponent(url)}?${qs.stringify(options)}`;
+	return `https://next-geebee.ft.com/image/v1/images/raw/${capiUUID ? `ftcms:${capiUUID}` : encodeURIComponent(url)}?${qs.stringify(options)}`;
 };
