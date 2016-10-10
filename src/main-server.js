a5d2cc725f7e374fd159710e4911f28ed2300645
Matt Hinchliffe <matt@maketea.co.uk>
2016-10-10 21:18:54 +0100

    Add comment explaining why this hack is necessary

--- src/main-server.js
+++ src/main-server.js
@@ -1,5 +1,7 @@
 import Image from './components/image';
 import Picture from './components/picture';
+
+// HACK: too many nested Babel interop require calls and this reference can get lost =[
 import * as _helpers from './helpers';
 
 export const helpers = _helpers;
