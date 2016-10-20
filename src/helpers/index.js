99370f32616111dcf084ff3973b6b049c7d25930
Dawn Budge <dawn.budge@ft.com>
2016-10-20 15:20:58 +0100

    Images data handling abstracted to presenter

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import breakpoints from '../helpers/breakpoints';
 import buildImageServiceUrl from '../helpers/build-image-service-url';
+import createImageSizes from '../helpers/create-image-sizes';
 
-export { breakpoints, buildImageServiceUrl };
+export { breakpoints, buildImageServiceUrl, createImageSizes };
