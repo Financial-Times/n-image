9fd403f180d5886a87b66a40dd83d9f2c5ea1427
andygnewman <andrew.newman@ft.com>
2016-11-29 10:32:18 +0000

    make -us non-capturing in regex

--- src/helpers/build-image-service-url.js
+++ src/helpers/build-image-service-url.js
@@ -1,6 +1,6 @@
 const qs = require('querystring');
 
-const capiRX = /^https?:\/\/(?:com\.ft\.imagepublish\.prod(-us)?\.s3\.amazonaws\.com|im\.ft-static\.com\/content\/images)\/([0-9a-f-]+)(?:\.img)?$/i
+const capiRX = /^https?:\/\/(?:com\.ft\.imagepublish\.prod(?:-us)?\.s3\.amazonaws\.com|im\.ft-static\.com\/content\/images)\/([0-9a-f-]+)(?:\.img)?$/i
 
 module.exports = (url, params = {}, { host = 'https://www.ft.com/__origami/service/image/v2/images/raw/' } = { }) => {
 	const defaultOptions = {
@@ -9,7 +9,7 @@ module.exports = (url, params = {}, { host = 'https://www.ft.com/__origami/servi
 		compression: 'best'
 	};
 	const options = Object.assign({}, defaultOptions, params);
-	const capiUUID = (capiRX.exec(url) || [])[2];
+	const capiUUID = (capiRX.exec(url) || [])[1];
 
 	return `${host + (capiUUID ? 'ftcms:'+capiUUID : encodeURIComponent(url))}?${qs.stringify(options)}`;
 };
