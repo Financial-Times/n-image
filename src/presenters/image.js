c4e30f701437cbae03a778a8bb1b5c26bd876330
Keith Rogers <git@keithrogers.eu>
2017-01-03 22:36:20 +0000

    Pass custom source parameter through to the image service url builder

--- src/presenters/image.js
+++ src/presenters/image.js
@@ -19,6 +19,7 @@ const createImageSizes = require('../helpers/create-image-sizes');
  * @param {string} [height] - Height of the image
  * @param {string} [alt = ''] - Alt text for the image
  * @param {boolean} [lazyLoad = false] - Lazy load the image
+ * @param {string} [sourceParam = 'next] - The `source` parameter used in the image service request
  * @param {string|number} [placeholder] - Set to add a placeholder. Value should be the ratio of the image as a number
  * (width divided by height, e.g.`16/9`), or string (`square` or `landscape`)
  */
@@ -127,6 +128,7 @@ class ImagePresenter {
 		const sizes = this.convertToJson(this.data.sizes) || this.imageSizes() || {};
 		const widths = this.convertToJson(this.data.widths) || [];
 		const srcSet = this.data.srcSet || this.data.url;
+		const sourceParam = this.data.sourceParam || 'next';
 
 		if (!this.data.src && !srcSet) {
 			console.warn('No source for image provided');
@@ -138,7 +140,7 @@ class ImagePresenter {
 			}
 			sourceAttrs.srcSet = widths
 				.sort((widthOne, widthTwo) => widthTwo - widthOne)
-				.map(width => `${buildImageServiceUrl(srcSet, { width })} ${width}w`)
+				.map(width => `${buildImageServiceUrl(srcSet, { width, source: sourceParam })} ${width}w`)
 				.join(', ');
 
 			sourceAttrs.sizes = breakpoints
